331a99b38db74c948fd059c0a56247818ab9a3e9
Vitaliy <shishovit@gmail.com>
2022-04-07 23:31:19 +0200

    fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,4 +16,24 @@ module.exports = {
     project: ['./tsconfig.json'],
   },
   plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'default-case': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'no-throw-literal': 'off',
+    'no-unused-vars': 'error',
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'react/jsx-boolean-value': ['error', 'always'],
+    'react/jsx-child-element-spacing': 'error',
+    'react/jsx-no-useless-fragment': 'error',
+    'react/no-unescaped-entities': 'error',
+    'react/no-unstable-nested-components': 'error',
+    'import/newline-after-import': 'error',
+    'newline-before-return': 'error',
+  },
 }
